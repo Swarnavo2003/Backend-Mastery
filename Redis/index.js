8c2d5d92cd3ec7826b895033bb0845cb7850891a
Swarnavo2003 <swarnavo2020@gmail.com>
2025-09-20 20:26:12 +0530

    Redis Day 1

---
+++ Redis/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import redis from "./redis.js";
+const app = express();
+
+app.get("/", async (req, res) => {
+  try {
+    const views = await redis.incr("views");
+    res.send(`This page was visited ${views} times`);
+  } catch (error) {
+    res.status(500).send("Error accessing Redis");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
